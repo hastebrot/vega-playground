16ccd20d9a31c426abe61e39e673176ab5845c72
Benjamin Gudehus <hastebrot@gmail.com>
2019-07-13 22:06:48 +0200

    (test) Add dataflow changeset tests.

--- src/dataflow.test.js
+++ src/dataflow.test.js
@@ -3,6 +3,7 @@ import * as vega from "vega-dataflow"
 
 describe("dataflow api", () => {
   test("default", () => {
+    // given:
     const flow = df.dataflow([
       df
         .aggregate()
@@ -10,12 +11,14 @@ describe("dataflow api", () => {
         .measure([df.count(), df.sum("amount").as("sum")]),
     ])
 
+    // when:
     flow.insert([
       { category: "a", amount: 12 },
       { category: "a", amount: 5 },
       { category: "b", amount: 11 },
     ])
 
+    // then:
     expect(flow.values()).toMatchInlineSnapshot(`
       Array [
         Object {
@@ -36,7 +39,8 @@ describe("dataflow api", () => {
 })
 
 describe("dataflow", () => {
-  test("success", () => {
+  test("propagate values", () => {
+    // given:
     const df = new vega.Dataflow()
 
     const s1 = df.add(10)
@@ -44,27 +48,78 @@ describe("dataflow", () => {
     const n1 = df.add(it => it.s1 + 0.25, { s1 })
     const n2 = df.add(it => it.n1 * it.s2, { n1, s2 })
     const op = [s1, s2, n1, n2]
-    const stamp = it => it.stamp
     expect(df.stamp()).toEqual(0)
 
+    // when:
     df.run()
+
+    // then:
     expect(df.stamp()).toEqual(1)
-    expect(op.map(stamp)).toEqual([1, 1, 1, 1])
+    expect(op.map(it => it.stamp)).toEqual([1, 1, 1, 1])
     expect(n2.value).toEqual(30.75) // (10 + 0.25) * 3.
   })
 
-  test("error", () => {
+  test("handle errors", () => {
+    // given:
     const df = new vega.Dataflow()
 
-    let error = 0
-    df.error = () => {
-      error = 1
+    let lastError = null
+    df.error = error => {
+      lastError = error
     }
     df.add(() => {
-      throw Error("!!!")
+      throw new Error("error")
     })
 
+    // when:
     df.run()
-    expect(error).toEqual(1)
+
+    // then:
+    expect(lastError).toEqual(new Error("error"))
+  })
+
+  test("changeset tuples", () => {
+    // given:
+    const data = [
+      { key: "a", value: 1 },
+      { key: "b", value: 2 },
+      { key: "c", value: 3 },
+      { key: "d", value: 6 },
+    ]
+
+    // when:
+    const pulse = vega
+      .changeset()
+      .insert(data)
+      .pulse(new vega.Pulse(), [])
+
+    // then:
+    expect(pulse.add).toEqual(data)
+    expect(pulse.rem).toEqual([])
+    expect(pulse.mod).toEqual([])
+    expect(data.every(vega.tupleid)).toBeTruthy()
+
+    // when:
+    const pulse2 = vega
+      .changeset()
+      .modify(data[0], "value", 5)
+      .pulse(new vega.Pulse(), data)
+
+    // then:
+    expect(pulse2.add).toEqual([])
+    expect(pulse2.rem).toEqual([])
+    expect(pulse2.mod).toEqual([data[0]])
+    expect(data[0].value).toEqual(5)
+
+    // when:
+    const pulse3 = vega
+      .changeset()
+      .remove(data[0])
+      .pulse(new vega.Pulse(), data)
+
+    // then:
+    expect(pulse3.add).toEqual([])
+    expect(pulse3.rem).toEqual([data[0]])
+    expect(pulse3.mod).toEqual([])
   })
 })
